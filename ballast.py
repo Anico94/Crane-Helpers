e243b0ec0d1d435fecaed5d5ff42d257da7aad23
Alex <a.nicolaidis@outlook.com>
2023-01-13 13:32:07 +1000

    Added use of pandas dataframe

--- ballast.py
+++ ballast.py
@@ -2,26 +2,39 @@ import math
 import pandas as pd
 
 class CalculateBallast:
-    #stab_moment in kNm , vetical load in kN, size is length of one side of the static base
-    def __init__(self, stab_moment, vertical, size):
-        self.stab_moment = stab_moment
+    #moment in kNm , vetical load in kN, size is length of one side of the static base
+    def __init__(self, moment, vertical, centres):
+        self.moment = moment
         self.vertical = vertical
-        self.size = size
+        self.centres = centres
 
-    def actions(self):
-        compression = self.stab_moment/(math.sqrt(2)*self.size) + self.vertical/4
-        tension = self.stab_moment/(math.sqrt(2)*self.size) - self.vertical/4
-        return [compression,tension]
+    def leg_actions(self):
+        compression45 = self.moment/(math.sqrt(2)*self.centres) + self.vertical/4
+        tension45 = self.moment/(math.sqrt(2)*self.centres) - self.vertical/4
+        neutral45 = self.vertical/4
+        compression90 = self.moment/(2*self.centres) + self.vertical/4
+        tension90 = self.moment/(2*self.centres) - self.vertical/4
+
+        leg_actions = [compression45, tension45,neutral45, compression90, tension90]
+
+
+        return leg_actions
 
 crane_1 = CalculateBallast(14100, 2360, 5)
 
-print(crane_1.actions())
+df = pd.DataFrame({"A": [crane_1.leg_actions()[3],crane_1.leg_actions()[2]],
+"B": [crane_1.leg_actions()[3],crane_1.leg_actions()[0]],
+"C": [crane_1.leg_actions()[1],crane_1.leg_actions()[2]],
+"D": [crane_1.leg_actions()[1],crane_1.leg_actions()[4]]
+}, index = [0,45])
+
+print(df)
 
 class ReactionTable:
-    def __init__(self, compression, tension, netural):
+    def __init__(self, compression, tension, neutral):
         compression = self.compression
         tension = self.tension
-        netural = self.netural
+        neutral = self.neutral
 
     
 
